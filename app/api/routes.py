d9279996bad96d11c24fd4c16afd84cae7589a2a
Hwang TaeYeon <ty5321@naver.com>
2026-05-09 02:53:34 +0900

    feat : import 누락 수정

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.challenge_routes import router as challenge_router
+from app.api.chemistry_routes import router as chemistry_router
 from app.api.crew_routes import router as crew_router
 
 router = APIRouter()
